3111c29dae892b50ae6bec24f9e226a855676e23
Shanuka Lakshan <121119338+SILVA-HSL@users.noreply.github.com>
2024-07-16 18:11:37 +0530

    initial commit

---
+++ myPortfolio/src/pages/home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./home.css";
+import Navbar from "../components/navbar";
+
+
+function Home() {
+  return (
+    <div>
+        <div className="mt-0 fixed-top ">   <Navbar /></div>
+      
+     
+    </div>
+  );
+}
+export default Home;
