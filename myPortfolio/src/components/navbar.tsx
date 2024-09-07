4fb6021abd99a1f149fd20d0adf15d142ffdca67
Shanuka Lakshan <121119338+SILVA-HSL@users.noreply.github.com>
2024-09-07 07:54:55 +0530

    change navbar, image view

--- myPortfolio/src/components/navbar.tsx
+++ myPortfolio/src/components/navbar.tsx
@@ -1,5 +1,40 @@
+// import React from "react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./navbar.css";
+
+// function Navbar() {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//       <div className="container-fluid">
+//         <a className="navbar-brand f" href="#" style={{fontFamily: 'fantasy', fontSize: '1.3em'}}>s h a n u k a</a>
+//         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className="collapse navbar-collapse " id="navbarNav">
+//           <ul className="navbar-nav">
+//             <li className="nav-item">
+//               <a className="nav-link active" aria-current="page" href="#" style={{background:'#98FB98',borderRadius:'50px'}}>Portfolio</a>
+//             </li>
+//             <li className="nav-item">
+//               <a className="nav-link" href="#about">About</a>
+//             </li>
+//             <li className="nav-item">
+//               <a className="nav-link" href="#contact">Contact</a>
+//             </li>
+           
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
+
+
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Ensure JS is included for collapse functionality
 import "./navbar.css";
 
 function Navbar() {
@@ -10,8 +45,8 @@ function Navbar() {
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse " id="navbarNav">
-          <ul className="navbar-nav">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#" style={{background:'#98FB98',borderRadius:'50px'}}>Portfolio</a>
             </li>
@@ -21,7 +56,6 @@ function Navbar() {
             <li className="nav-item">
               <a className="nav-link" href="#contact">Contact</a>
             </li>
-           
           </ul>
         </div>
       </div>
